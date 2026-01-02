b804f76d58354efbb25825fc138efc59b3ca1b4d
Tashfeen-Khan <tashfeenkhan158@gmail.com>
2026-01-02 23:40:15 +0500

    complete side bar

---
+++ app/home/page.tsx
@@ -0,0 +1,16 @@
+import { ContentWrapper } from '@/customComponent/contentwraper'
+import NavBar from '@/customComponent/navBar'
+
+
+const page = () => {
+  return (
+    <>
+        <NavBar/>
+      <ContentWrapper>
+        <h1>hellow</h1>
+      </ContentWrapper>
+    </>
+  )
+}
+
+export default page
---
+++ app/products/page.tsx
@@ -0,0 +1,15 @@
+import { ContentWrapper } from '@/customComponent/contentwraper'
+import NavBar from '@/customComponent/navBar'
+
+const page = () => {
+  return (
+    <>
+    <NavBar/>
+        <ContentWrapper>
+            <h1>hellow content</h1>
+        </ContentWrapper>
+    </>
+  )
+}
+
+export default page
